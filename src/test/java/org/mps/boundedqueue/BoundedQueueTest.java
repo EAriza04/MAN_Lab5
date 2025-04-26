65f71d1b7de6c726c57ecc8dc2817f9391aac5a3
Eduardo <earizaabad@gmail.com>
2025-04-26 17:27:14 +0200

    Monicius

--- src/test/java/org/mps/boundedqueue/BoundedQueueTest.java
+++ src/test/java/org/mps/boundedqueue/BoundedQueueTest.java
@@ -54,4 +54,135 @@ public class BoundedQueueTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("put: element cannot be null");
     }
+    
+    @Test
+    @DisplayName("get() throws EmptyBoundedQueueException if the queue is empty")
+    public void get_EmptyQueue_ThrowsEmptyBoundedQueueException() {
+        // Act + Assert
+        assertThatThrownBy(() -> queue.get())
+            .isInstanceOf(EmptyBoundedQueueException.class)
+            .hasMessageContaining("get: empty bounded queue");
+    }
+
+    @Test
+    @DisplayName("get() returns the first element in the queue")
+    public void get_NonEmptyQueue_ReturnsFirstElement() {
+        // Arrange
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+
+        // Act
+        int result = queue.get();
+        int expected = 1;
+
+        // Assert
+        assertThat(result)
+            .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("isFull() returns true if the queue is full")
+    public void isFull_FullQueue_ReturnsTrue() {
+        // Arrange
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+        queue.put(4);
+        queue.put(5);
+
+        // Act
+        boolean result = queue.isFull();
+
+        // Assert
+        assertThat(result)
+            .isTrue();
+    }
+
+    @Test
+    @DisplayName("isFull() returns false if the queue is not full")
+    public void isFull_NonFullQueue_ReturnsFalse() {
+        // Arrange
+        queue.put(1);
+        queue.put(2);
+
+        // Act
+        boolean result = queue.isFull();
+
+        // Assert
+        assertThat(result)
+            .isFalse();
+    }
+
+    @Test
+    @DisplayName("isEmpty() returns true if the queue is empty")
+    public void isEmpty_EmptyQueue_ReturnsTrue() {
+        // Act
+        boolean result = queue.isEmpty();
+
+        // Assert
+        assertThat(result)
+            .isTrue();
+    }
+
+    @Test
+    @DisplayName("isEmpty() returns false if the queue is not empty")
+    public void isEmpty_NonEmptyQueue_ReturnsFalse() {
+        // Arrange
+        queue.put(1);
+
+        // Act
+        boolean result = queue.isEmpty();
+
+        // Assert
+        assertThat(result)
+            .isFalse();
+    }
+
+    @Test
+    @DisplayName("size() returns the number of elements in the queue")
+    public void size_NonEmptyQueue_ReturnsCorrectSize() {
+        // Arrange
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+
+        // Act
+        int result = queue.size();
+        int expected = 3;
+
+        // Assert
+        assertThat(result)
+            .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("getFirst() returns the index of the position of the first element")
+    public void getFirst_Queue_ReturnsFirstIndex() {
+        // Act
+        int result = queue.getFirst();
+        int expected = 0;
+
+        // Assert
+        assertThat(result)
+            .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("getLast() returns the index of the position of the last element")
+    public void getLast_Queue_ReturnsLastIndex() {
+        // Arrange
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+
+        // Act
+        int result = queue.getLast();
+        int expected = 3;
+
+        // Assert
+        assertThat(result)
+            .isEqualTo(expected);
+    }
+
 }
