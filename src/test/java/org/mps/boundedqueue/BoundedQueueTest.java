d6423fe428df8ee0917fe9de184519a513100b14
Enri238 <enrihomipat@icloud.com>
2025-04-26 16:46:26 +0200

    setUp()

--- src/test/java/org/mps/boundedqueue/BoundedQueueTest.java
+++ src/test/java/org/mps/boundedqueue/BoundedQueueTest.java
@@ -1,5 +1,14 @@
 package org.mps.boundedqueue;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class BoundedQueueTest {
+
+    ArrayBoundedQueue<Integer> queue;
+    
+    @BeforeEach
+    public void setUp() {
+        queue = new ArrayBoundedQueue<>(5);
+    }
 
 }
